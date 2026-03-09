34fe9b74c409e99ed40d3b27f67d1cfaa6f68b41
haf5th <hafiflearning@gmail.com>
2026-03-09 17:48:24 +0700

    Jobsheet 4

---
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Filament\Resources\Posts\Schemas;
+
+use Filament\Schemas\Schema;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\ColorPicker;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\RichEditor;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\Checkbox;
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TagsInput;
+
+
+class PostForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                //
+                TextInput::make("title")->required(),
+                TextInput::make("slug")->required(),
+                Select::make("category_id")
+                    ->relationship("category", "name")
+                    ->preload()
+                    ->searchable(),
+                ColorPicker::make("color"),
+                MarkdownEditor::make("body"),
+                //RichEditor::make("body"),
+                FileUpload::make("image")
+                ->disk("public")
+                ->directory("posts"),
+                TagsInput::make("tags"),
+                Checkbox::make("published"), 
+                DateTimePicker::make("published_at"),
+                
+                
+            ]);
+    }
+}
