a89189b2521fc1aa115953afcee7a735e0ea6038
haf5th <hafiflearning@gmail.com>
2026-03-30 16:51:51 +0700

    Jobsheet 10

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -15,56 +15,58 @@ use Filament\Schemas\Components\Section;
 use Filament\Schemas\Components\Group;
 use Illuminate\Validation\Rules\Unique;
 
-
 class PostForm
 {
     public static function configure(Schema $schema): Schema
     {
         return $schema
             ->components([
-
-                Section::make('Post Details')
-                    ->description('Fill in the details of the post.')
-                    ->icon('heroicon-o-document-text')
+                Section::make("Post Details")
+                    ->description("Fill in the details of the post.")
+                    ->icon("heroicon-o-document-text")
                     ->schema([
                         Group::make([
-                            TextInput::make('title')
-                                ->rules('required | min:3 | max:10')
+                            TextInput::make("title")
+                                ->rules("required | min:3 | max:10")
                                 ->maxLength(255),
-                            TextInput::make('slug')
-                                ->rules('required ')
+                            TextInput::make("slug")
+                                ->rules("required ")
                                 ->unique()
                                 ->validationMessages([
-                                    'unique' => 'Slug harus unik dan tidak boleh sama.'
+                                    "unique" =>
+                                        "Slug harus unik dan tidak boleh sama.",
                                 ]),
-                            Select::make('category_id')
-                                ->label('Category')
-                                ->relationship('category', 'name')
+                            Select::make("category_id")
+                                ->label("Category")
+                                ->relationship("category", "name")
                                 ->required()
                                 ->preload()
                                 ->searchable(),
-                            ColorPicker::make('color'),
+                            ColorPicker::make("color"),
                         ])->columns(2),
-                        MarkdownEditor::make('content'),
-                    ])->columnSpan(2),
+                        MarkdownEditor::make("body")->label("Body")->required(),
+                    ])
+                    ->columnSpan(2),
 
                 Group::make([
-                    Section::make('Image Upload')->schema([
-                        FileUpload::make('image')
+                    Section::make("Image Upload")->schema([
+                        FileUpload::make("image")
                             ->required()
-                            ->disk('public')
-                            ->directory('posts'),
+                            ->disk("public")
+                            ->directory("posts"),
                     ]),
 
-                    Section::make('Meta Information')->schema([
-                        // RichEditor::make('content'),
-                        TagsInput::make('tags'),
-                        Checkbox::make('published'),
-                    ])->columns(1),
+                    Section::make("Meta Information")
+                        ->schema([
+                            // RichEditor::make('content'),
+                            TagsInput::make("tags"),
+                            Checkbox::make("published"),
+                        ])
+                        ->columns(1),
 
-                    DateTimePicker::make('published_at'),
+                    DateTimePicker::make("published_at"),
                 ]),
-
-            ])->columns(3);
+            ])
+            ->columns(3);
     }
 }
